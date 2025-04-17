4f00e427c76bdf0985f1ad00c9b6dc7e22c20454
sanyamkashyap <sanyamkashyap40@gmail.com>
2025-04-17 12:15:23 +0530

    product showcase comp

--- sections/ProductShowcase.tsx
+++ sections/ProductShowcase.tsx
@@ -1,11 +1,16 @@
+import productImage from "@/assets/product-image.png"
+import Image from "next/image";
+
 export const ProductShowcase = () => {
   return (
-    <section>
+    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
       <div className="container">
         <div>Boost your poductivity</div>
         <h2>A more effective way to track progress</h2>
         <p>Effortlessly turn your ideas into a fully functional, responsive, SaaS website in just with this template</p>
+        <Image src={productImage} alt="product image" className=""></Image>
       </div>
     </section>
   );
 };
+
