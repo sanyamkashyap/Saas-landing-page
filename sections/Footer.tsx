8a580d09b1e7443612479e3aab46d855eba618f5
sanyamkashyap <sanyamkashyap40@gmail.com>
2025-08-23 10:10:24 +0530

    add animation

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,8 +1,10 @@
-import logo from "@/assets/logosaas.png"
+import logo from "@/assets/logosaas.png";
 import Image from "next/image";
-import SocialX from "@/assets/social-x.svg"
-import SocialInsta from "@/assets/social-insta.svg"
-import SocialInsta from "@/assets/social-insta.svg"
+import SocialX from "@/assets/social-x.svg";
+import SocialInsta from "@/assets/social-insta.svg";
+import SocialLinkedIn from "@/assets/social-linkedin.svg"
+import SocialYoutube from "@/assets/social-youtube.svg"
+import SocialPin from "@/assets/social-pin.svg"
 
 const footerLinks = [
   {
@@ -29,15 +31,38 @@ export const Footer = () => {
   return (
     <footer className="bg-black text-white px-6">
       <div className="container">
-        <div>
-          <div>
+        <div className="py-8">
+          <div className="flex justify-between gap-4">
             <div>
-            <Image src={logo} width={40} height={40} alt="logo" className="mb-4"></Image>
-            <p className="text-sm text-gray-400 mb-4">Effortlessly turn your ideas into a fully functional, responsive, SaaS website in just with this template</p>
+              <div>
+                <Image src={logo} width={40} height={40} alt="logo" className="mb-4"></Image>
+                <p className="text-sm text-gray-400 mb-4 w-60">Effortlessly turn your ideas into a fully functional, responsive, SaaS website in just with this template</p>
+              </div>
+              <div className="flex gap-2">
+                <SocialInsta />
+                <SocialX />
+                <SocialLinkedIn />
+                <SocialPin />
+                <SocialYoutube />
+              </div>
             </div>
-            <div>
 
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+              {footerLinks.map((section, index) => (
+                <div key={index}>
+                  <h4>{section.title}</h4>
+                  <ul>
+                    {section.links.map((link, i) => (
+                      <li key={i}>
+                        <a href="#" className="text-gray-400 hover:text-white">{link}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))
+              }
             </div>
+
           </div>
         </div>
       </div>
--- sections/Pricing.tsx
+++ sections/Pricing.tsx
@@ -1,5 +1,7 @@
+"use client"
 import CheckIcon from "@/assets/check.svg";
-import  {twMerge}  from "tailwind-merge";
+import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion"
 
 const pricingTiers = [
   {
@@ -52,24 +54,35 @@ const pricingTiers = [
     ],
   },
 ];
- 
+
 export const Pricing = () => {
   return (
     <section className="py-24 bg-white">
       <div className="container">
         <h2 className="section-title">Pricing </h2>
         <div className="section-heading">
-        <p className="section-description mt-5">Free forever, Upgrage for unlimited tasks, better security,and exclusive features,</p>
+          <p className="section-description mt-5">Free forever, Upgrage for unlimited tasks, better security,and exclusive features,</p>
         </div>
-        
+
         <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end">
-          {pricingTiers.map(({ title, monthlyPrice, buttonText, popular, inverse, features },index) => (
-            <div key={index} className={twMerge("card",inverse === true && 'border-black bg-black text-white')}>
+          {pricingTiers.map(({ title, monthlyPrice, buttonText, popular, inverse, features }, index) => (
+            <div key={index} className={twMerge("card", inverse === true && 'border-black bg-black text-white')}>
               <div className="flex justify-between">
-                <h3 className={twMerge("text-lg font-bold text-black/50",inverse === true && 'text-white/60')}>{title}</h3>
+                <h3 className={twMerge("text-lg font-bold text-black/50", inverse === true && 'text-white/60')}>{title}</h3>
                 {popular === true && (
                   <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
-                    <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">Popular</span>
+                    <motion.span
+                      animate={{
+                        backgroundPositionX: '100%',
+                      }}
+                      transition={{
+                        duration:1,
+                        repeat: Infinity,
+                        ease: "linear",
+                        repeatType: "loop"
+                      }}
+                      className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] [background-size:200%] text-transparent bg-clip-text font-medium">Popular
+                    </motion.span>
                   </div>
                 )}
               </div>
@@ -77,10 +90,10 @@ export const Pricing = () => {
                 <span className="text-4xl font-bold tracking-tighter leading-none">${monthlyPrice}</span>
                 <span className="tracking-tight font-bold text-black/50">/month</span>
               </div>
-              <button className={twMerge("btn btn-primary w-full mt-[30px]",inverse === true && "bg-white text-black font-bold")}>{buttonText}</button>
+              <button className={twMerge("btn btn-primary w-full mt-[30px]", inverse === true && "bg-white text-black font-bold")}>{buttonText}</button>
               <ul className="flex flex-col gap-5 mt-8">
 
-                {features.map((feature,index) => (
+                {features.map((feature, index) => (
                   <li key={index} className="text-sm flex items-center gap-4">
                     <CheckIcon className="h-5 w-5"></CheckIcon>
                     <span>
